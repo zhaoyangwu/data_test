2a485c7fc7c3b818c31532eef9389da32c486e84
zhaoyangwu <625036860@qq.com>
2018-10-23 12:12:35 +0800

    test

---
+++ src/TestHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zyw
+ * Date: 2018-10-23
+ * Time: 12:08
+ */
+
+namespace TestHello;
+
+
+class TestHello
+{
+    public static function index(){
+        echo "hello world";
+    }
+}
